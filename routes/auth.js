76a9d71c847e0fc84d4fde6c924c427e1e47207b
Shoko <52348865+key0123456789@users.noreply.github.com>
2022-11-11 03:33:24 +0900

    bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
   res.send('auth router');
@@ -11,7 +12,7 @@ router.post('/register', async (req, res) => {
     const newUser = await new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password
+      password: await bcrypt.hash(req.body.password, 10)
     })
 
     const user = await newUser.save();
@@ -27,7 +28,7 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(404).send('ユーザーが見つかりません');
 
-    const veiledPassword = req.body.password === user.password;
+    const veiledPassword = bcrypt.compare(user.password);
     if (!veiledPassword) return res.status(400).json('パスワードが違います')
 
     return res.status(200).json(user);
